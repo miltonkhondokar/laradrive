f685828ba64329a4fcd6f1295e9c0a4cf9f67252
Milton Khondokar <miltonkhondokar52@gmail.com>
2022-02-09 11:48:36 +0600

    Update dashboard.blade.php

--- resources/views/file-manager/pages/dashboard.blade.php
+++ resources/views/file-manager/pages/dashboard.blade.php
@@ -19,7 +19,7 @@
                     <div class="card-body pt-0">
 
                         <div class="row" id="suggestDiv">
-                            <i class="nav-icon far fa-file fa-5x" style="margin: 0 auto !important; padding-top: 18px !important;"></i>
+                            <i class="nav-icon far fa-file-pdf fa-5x" style="margin: 0 auto !important; padding-top: 18px !important;"></i>
                             <i class="fas fa-ellipsis-v" style="color: #bbbbbbed !important;"></i>
                         </div>
                     </div>
@@ -39,7 +39,7 @@
                     <div class="card-body pt-0">
 
                         <div class="row" id="suggestDiv">
-                            <i class="nav-icon far fa-file fa-5x" style="margin: 0 auto !important; padding-top: 18px !important;"></i>
+                            <i class="nav-icon far fa-file-word fa-5x" style="margin: 0 auto !important; padding-top: 18px !important;"></i>
                             <i class="fas fa-ellipsis-v" style="color: #bbbbbbed !important;"></i>
                         </div>
                     </div>
@@ -59,7 +59,7 @@
                     <div class="card-body pt-0">
 
                         <div class="row" id="suggestDiv">
-                            <i class="nav-icon far fa-file fa-5x" style="margin: 0 auto !important; padding-top: 18px !important;"></i>
+                            <i class="nav-icon far fa-file-image fa-5x" style="margin: 0 auto !important; padding-top: 18px !important;"></i>
                             <i class="fas fa-ellipsis-v" style="color: #bbbbbbed !important;"></i>
                         </div>
                     </div>
@@ -79,7 +79,7 @@
                     <div class="card-body pt-0">
 
                         <div class="row" id="suggestDiv">
-                            <i class="nav-icon far fa-file fa-5x" style="margin: 0 auto !important; padding-top: 18px !important;"></i>
+                            <i class="nav-icon far fa-file-audio fa-5x" style="margin: 0 auto !important; padding-top: 18px !important;"></i>
                             <i class="fas fa-ellipsis-v" style="color: #bbbbbbed !important;"></i>
                         </div>
                     </div>
@@ -99,7 +99,7 @@
                     <div class="card-body pt-0">
 
                         <div class="row" id="suggestDiv">
-                            <i class="nav-icon far fa-file fa-5x" style="margin: 0 auto !important; padding-top: 18px !important;"></i>
+                            <i class="nav-icon far fa-file-video fa-5x" style="margin: 0 auto !important; padding-top: 18px !important;"></i>
                             <i class="fas fa-ellipsis-v" style="color: #bbbbbbed !important;"></i>
                         </div>
                     </div>
@@ -119,7 +119,8 @@
                     <div class="card-body pt-0">
 
                         <div class="row" id="suggestDiv">
-                            <i class="nav-icon far fa-file fa-5x" style="margin: 0 auto !important; padding-top: 18px !important;"></i>
+                            {{--<i class="fas fa-file-csv"></i>--}}
+                            <i class="nav-icon far fa-file-excel fa-5x" style="margin: 0 auto !important; padding-top: 18px !important;"></i>
                             <i class="fas fa-ellipsis-v" style="color: #bbbbbbed !important;"></i>
                         </div>
                     </div>
