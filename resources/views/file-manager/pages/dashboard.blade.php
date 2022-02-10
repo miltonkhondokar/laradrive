4a4f5469594ba0e8caed929e7ccdc350174c79d7
Milton Khondokar <miltonkhondokar52@gmail.com>
2022-02-10 12:01:55 +0600

    right click menu fixed

--- resources/views/file-manager/pages/dashboard.blade.php
+++ resources/views/file-manager/pages/dashboard.blade.php
@@ -11,19 +11,6 @@
 <section class="content">
     <div class="container-fluid">
 
-        <div class="right-click-menu">
-            <ul>
-                <li><i class="nav-icon far fa-star"></i><p>Mark as important</p></li>
-                <li><i class="nav-icon far fa-star"></i><p>Mark as important</p></li>
-                <li>Share</li>
-                <li>Copy To</li>
-                <li>Move To</li>
-                <li>Delete</li>
-                <li>Rename</li>
-                <li>Download</li>
-                <li>View Details</li>
-            </ul>
-        </div>
 
 
         <div class="padding-top"></div>
@@ -151,7 +138,27 @@
         <h2 id="bodyLabel">Folders</h2>
         <div class="row">
 
-            <div class="col-sm-2">
+            <div class="col-sm-3 box">
+                <div class="info-box bg-light">
+                    <i class="nav-icon fas fa-folder fa-4x folder-color" style="padding-left: 10px;"></i>
+                    <div class="info-box-content" style="overflow: hidden;">
+                        <span class="info-box-text text-left text-muted"style="padding-left: 10px;">Test Folder</span>
+                        <span class="info-box-number text-left text-muted mb-0"style="padding-left: 10px;"></span>
+                    </div>
+                    <i class="fas fa-ellipsis-v" style="color: #bbbbbbed !important;"></i>
+                </div>
+                <div id="contextMenu" class="context-menu">
+                    <ul class="menu">
+                        <li class="addToStarred"><a href="#"><i class="nav-icon far fa-star" aria-hidden="true" style="padding-right: 5px;"></i> Add to Starred</a></li>
+                        <li class="rename" data-toggle="modal" data-target="#exampleModalCenter"><a href="#"><i class="nav-icon far fa-edit" aria-hidden="true" style="padding-right: 5px;"></i> Rename</a></li>
+                        <li class="move"><a href="#"><i class="nav-icon fas fa-cut" aria-hidden="true" style="padding-right: 5px;"></i> Move to</a></li>
+                        <li class="download"  style="border-top: 1px solid lightgrey;"><a href="#"><i class="nav-icon fas fa-download" aria-hidden="true" style="padding-right: 5px;"></i> Download</a></li>
+                        <li class="viewDetails"><a href="#"><i class="nav-icon fas fa-info-circle" aria-hidden="true" style="padding-right: 5px;"></i> View Details</a></li>
+                        <li class="trash"  style="border-top: 1px solid lightgrey;"><a href="#"><i class="nav-icon far fa-trash-alt" aria-hidden="true" style="padding-right: 5px;"></i> Remove</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-3">
                 <div class="info-box bg-light">
                     <i class="nav-icon fas fa-folder fa-4x folder-color" style="padding-left: 10px;"></i>
                     <div class="info-box-content" style="overflow: hidden;">
@@ -162,7 +169,7 @@
                 </div>
             </div>
 
-            <div class="col-sm-2">
+            <div class="col-sm-3">
                 <div class="info-box bg-light">
                     <i class="nav-icon fas fa-folder fa-4x folder-color" style="padding-left: 10px;"></i>
                     <div class="info-box-content" style="overflow: hidden;">
@@ -173,40 +180,7 @@
                 </div>
             </div>
 
-            <div class="col-sm-2">
-                <div class="info-box bg-light">
-                    <i class="nav-icon fas fa-folder fa-4x folder-color" style="padding-left: 10px;"></i>
-                    <div class="info-box-content" style="overflow: hidden;">
-                        <span class="info-box-text text-left text-muted"style="padding-left: 10px;">Test Folder</span>
-                        <span class="info-box-number text-left text-muted mb-0"style="padding-left: 10px;"></span>
-                    </div>
-                    <i class="fas fa-ellipsis-v" style="color: #bbbbbbed !important;"></i>
-                </div>
-            </div>
-
-            <div class="col-sm-2">
-                <div class="info-box bg-light">
-                    <i class="nav-icon fas fa-folder fa-4x folder-color" style="padding-left: 10px;"></i>
-                    <div class="info-box-content" style="overflow: hidden;">
-                        <span class="info-box-text text-left text-muted"style="padding-left: 10px;">Test Folder</span>
-                        <span class="info-box-number text-left text-muted mb-0"style="padding-left: 10px;"></span>
-                    </div>
-                    <i class="fas fa-ellipsis-v" style="color: #bbbbbbed !important;"></i>
-                </div>
-            </div>
-
-            <div class="col-sm-2">
-                <div class="info-box bg-light">
-                    <i class="nav-icon fas fa-folder fa-4x folder-color" style="padding-left: 10px;"></i>
-                    <div class="info-box-content" style="overflow: hidden;">
-                        <span class="info-box-text text-left text-muted"style="padding-left: 10px;">Test Folder</span>
-                        <span class="info-box-number text-left text-muted mb-0"style="padding-left: 10px;"></span>
-                    </div>
-                    <i class="fas fa-ellipsis-v" style="color: #bbbbbbed !important;"></i>
-                </div>
-            </div>
-
-            <div class="col-sm-2">
+            <div class="col-sm-3">
                 <div class="info-box bg-light">
                     <i class="nav-icon fas fa-folder fa-4x folder-color" style="padding-left: 10px;"></i>
                     <div class="info-box-content" style="overflow: hidden;">
@@ -347,6 +321,49 @@
         </div>
 
 
+    </div>
+
+    <script>
+document.querySelector(".box").addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+    var contextMenu = document.querySelector(".context-menu");
+    posX = (e.clientX + 150 > document.documentElement.clientWidth) ? e.clientX - 150 : e.clientX,
+        posY = (e.clientY + 140 + 55 > document.documentElement.clientHeight) ? e.clientY - 140 : e.clientY;
+    contextMenu.style.top = posY + "px";
+    contextMenu.style.left = posX + "px";
+    contextMenu.classList.add("shown");
+});
+
+
+document.addEventListener("click", function (e) {
+    var elems2hide = document.querySelectorAll(".shown");
+    for (var i = 0, length = elems2hide.length; i < length; i++) {
+        elems2hide[i].classList.remove("shown");
+    }
+});
+    </script>
+
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 @endsection
